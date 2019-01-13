327df2c41790e3f8d74c34256af54f09b6a4f8ea
Unknown <Brandonjorgen@gmail.com>
2019-01-12 21:37:47 -0700

    Prototype Dev
    Movement and Turret Aiming

---
+++ Tank Game/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    private CharacterController controller;
+    public float Gravity = 9.81f;
+    public float MoveSpeed = 3.0f;
+    private Vector3 position;
+    private Vector3 rotation;
+    
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (controller.isGrounded)
+        {
+            
+            position.Set(MoveSpeed * Input.GetAxis("Horizontal"), 0, MoveSpeed * Input.GetAxis("Vertical"));
+            //transform.Rotate(rotation);
+            //rotation.Set(0, RotateSpeed * Input.GetAxis("Horizontal"), 0);
+            position = transform.TransformDirection(position);
+        }
+        position.y -= Gravity * Time.deltaTime;
+        controller.Move(position * Time.deltaTime);
+    }
+}
---
+++ Tank Game/Assets/Scripts/LookAtMouse.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour
+{
+    private Camera gameCamera;
+    
+    private Ray ray;
+    private Plane groundPlane;
+    private float rayDistance;
+
+    private Vector3 point;
+    private Vector3 correctPoint;
+
+    void Start()
+    {
+        gameCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        //Set up the raycast and create something for the ray to hit
+        ray = gameCamera.ScreenPointToRay(Input.mousePosition);
+        groundPlane = new Plane(Vector3.up, 0); //Apparently this is better/quicker to do than
+                                                //setting up the plane the player is already on?
+        
+        //if the ray hits something, report the distance from the camera to the collision
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            point = ray.GetPoint(rayDistance);
+            Debug.DrawLine(ray.origin, point, Color.red);
+            correctPoint = new Vector3(point.x, transform.position.y, point.z); //adjust transform for Y height
+            transform.LookAt(correctPoint); //look in the direction of the ray collision
+        }
+    }
+}
