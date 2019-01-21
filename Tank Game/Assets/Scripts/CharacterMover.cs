2e9d1813c0c9cdeac0a0d0888a0c4b9138a74e90
Unknown <Brandonjorgen@gmail.com>
2019-01-20 17:32:22 -0700

    Scripting
    Player Movement and Enemy AI

--- Tank Game/Assets/Scripts/CharacterMover.cs
+++ Tank Game/Assets/Scripts/CharacterMover.cs
@@ -1,10 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class CharacterMover : MonoBehaviour
 {
     private CharacterController controller;
     public float Gravity = 9.81f;
     public float MoveSpeed = 3.0f;
+    private string currentDirection;
     private Vector3 position;
     private Vector3 rotation;
     
@@ -15,13 +16,45 @@ public class CharacterMover : MonoBehaviour
 
     private void Update()
     {
-        if (controller.isGrounded)
+
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            
-            position.Set(MoveSpeed * Input.GetAxis("Horizontal"), 0, MoveSpeed * Input.GetAxis("Vertical"));
-            //transform.Rotate(rotation);
-            //rotation.Set(0, RotateSpeed * Input.GetAxis("Horizontal"), 0);
-            position = transform.TransformDirection(position);
+            currentDirection = "Left";
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            currentDirection = "Right";
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            currentDirection = "Up";
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            currentDirection = "Down";
+        }
+
+        switch (currentDirection)
+        {
+                case "Right":
+                    position.Set(MoveSpeed * Input.GetAxisRaw("Horizontal"), 0, 0);
+                    position = transform.TransformDirection(position);
+                    break;
+                case "Left":
+                    position.Set(MoveSpeed * Input.GetAxisRaw("Horizontal"), 0, 0);
+                    position = transform.TransformDirection(position);
+                    break;
+                case "Up":
+                    position.Set(0, 0, MoveSpeed * Input.GetAxisRaw("Vertical"));
+                    position = transform.TransformDirection(position);
+                    break;
+                case "Down":
+                    position.Set(0, 0, MoveSpeed * Input.GetAxisRaw("Vertical"));
+                    position = transform.TransformDirection(position);
+                    break;
         }
         position.y -= Gravity * Time.deltaTime;
         controller.Move(position * Time.deltaTime);
---
+++ Tank Game/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform[] Locs;
+    private NavMeshAgent agent;
+    private int destinationLoc = 0;
+    
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        NextLocation();
+    }
+
+    void NextLocation()
+    {
+        agent.destination = Locs[destinationLoc].position; //Tells the agent to go to the location in the array
+        destinationLoc = (destinationLoc + 1) % Locs.Length; //Sets up the next destination for the next func call
+                                                             //Also resets to the beginning of the array if it reaches the end
+    }
+
+    void Update()
+    {
+        if (agent.remainingDistance < 0.25f)
+        {
+            NextLocation();
+        }
+    }
+}
